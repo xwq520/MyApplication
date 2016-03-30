00edc5fd7c3253666e082aef21abcc919e39f750
xwq520 <250504235@qq.com>
2016-03-30 23:27:45 +0800

    顶顶顶顶顶顶顶

--- app/src/androidTest/java/com/example/xwqaa/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/xwqaa/myapplication/ApplicationTest.java
@@ -8,6 +8,6 @@ import android.test.ApplicationTestCase;
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
-        super(Application.class);//cegjid反反复复反反复复 adafdja fla
+        super(Application.class);//cddd大飒飒的答复的撒大egjid反反复复反反复复 adafdja fla
     }
 }
